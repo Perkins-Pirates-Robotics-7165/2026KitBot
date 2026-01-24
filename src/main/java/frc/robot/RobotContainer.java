b5061a58c73fc103861e913cc755c05b3abc272b
pkncoder <alex.h.preston@gmail.com>
2026-01-24 12:13:29 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  // Controller
+  private final CommandXboxController primary = new CommandXboxController(0);
+
+  // The start of robot container used to configure bindings and any initializing settup
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  // Set bindings for controllers / other set triggers
+  private void configureBindings() {
+
+  }
+
+  // Ran periodicly (called in Robot.java)
+  public void periodic() {
+
+  }
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
